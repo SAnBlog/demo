c3e3b77d5c8c86087a7080a6d1b8a3ac67ab5956
wangshouliang <1300100082@qq.com>
2020-08-12 16:21:31 +0800

    jenkins demo

---
+++ src/main/java/cn/sanii/demo/controller/HelloController.java
@@ -0,0 +1,23 @@
+package cn.sanii.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author shouliang.wang
+ * @date 2020/8/12 0012 16:13
+ * @des
+ */
+@Controller
+public class HelloController {
+
+
+    @ResponseBody
+    @GetMapping("/hi")
+    public String hi(){
+        return "hi jenkins";
+    }
+
+
+}
